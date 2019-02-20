264b9952e5a18ec44316f46026709864df1b22d2
Efstathios D. Gennatas <Efstathios.Gennatas@ucsf.edu>
2019-02-20 09:00:03 +0000

    version 0.1.0

---
+++ R/music_fn.R
@@ -0,0 +1,146 @@
+# music_fn.R
+# ::music::
+# 2019 Efstathios D. Gennatas egenn.github.io
+
+#' Format notes
+#'
+#' Format notes for use in other \pkg{music} functions
+#'
+#' Converts sharps to flats, adds octave number if missing (Default = 4), and converts (rare) "bb" notes
+#' to regular notes
+#'
+#' @param notes Vector, String: Input notes in the form \code{c("C4", "D4", "Eb4")}
+#' @param default.octave Integer: Octave to use if missing in \code{notes}. Default = 4; i.e. \code{"C"}
+#' becomes \code{"C4"}
+#' @examples
+#' formatNote(c("D#4", "Ebb"))
+#' @author Efstathios D. Gennatas
+#' @export
+
+formatNote <- function(notes, default.octave = 4) {
+
+  # Sharps to flats ====
+  index <- grep("#", notes)
+  notes[index] <- gsub("A", "B", notes[index])
+  notes[index] <- gsub("G", "A", notes[index])
+  notes[index] <- gsub("F", "G", notes[index])
+  notes[index] <- gsub("D", "E", notes[index])
+  notes[index] <- gsub("C", "D", notes[index])
+  notes[index] <- gsub("#", "b", notes[index])
+
+  # Add octave if missing
+  index <- seq(notes)
+  neg.index <- grep(paste(1:10, collapse = "|"), notes)
+  index <- setdiff(index, neg.index)
+  notes[index] <- paste0(notes[index], default.octave)
+
+  # bb to regular notes ====
+  index <- grep("bb", notes)
+  for (i in index) {
+    .noteup <- gsub("bb", "", notes[i])
+    notes[i] <- names(pos)[which(names(pos) == .noteup) - 2]
+  }
+
+  notes
+
+} # formatNote.R
+
+
+#' Note distance in semitones
+#'
+#' Calculates note distance in semitones
+#'
+#' @param notes String, vector: Notes in form \code{c("C4", "Eb4", "Gb4")}
+#' @return Vector of length \code{length(notes)} with semitone distances between successive notes
+#' @examples
+#' noteDistance(strings("C4 Eb4 Gb4 Bb4"))
+#' @author Efstathios D. Gennatas
+#' @export
+
+noteDistance <- function(notes) {
+
+  notes <- formatNote(notes)
+  .diff <- diff(pos[notes])
+  .diff <- c(0, .diff)
+  names(.diff)[1] <- notes[1]
+  .diff
+
+} # music:noteDistance
+
+
+#' Separate notes into vector of strings
+#'
+#' Convenience function to separate notes into vector of strings
+#'
+#' Makes it easy to copy-paste notes into other functions
+#' e.g. \code{playChord(strings("C4 Eb4 G4 D5"))}
+#' @param x String: A single character object which consists of multiple notes separated by \code{sep}
+#' e.g. \code{"C4 Eb4 G4 D5"}
+#' @param sep String: the character that separates notes in \code{x}. Default = " "
+#' @examples
+#' strings("C4 Eb4 Gb4 Bb4")
+#' @author Efstathios D. Gennatas
+#' @export
+
+strings <- function(x, sep = " ") {
+
+  strsplit(x, split = sep)[[1]]
+
+} # music::strings
+
+
+#' Format Notation
+#'
+#' Converts the internal note representation which uses only flats, to the notation
+#' commonly used to write scales and chords, where a mix of sharps and flats is used to avoid
+#' repeating the same letter note. (e.g. \code{"G#5" "A5"}, instead of \code{"Ab5" "A5"})
+#' e.g. convert the C4 Lydian from:
+#' "C4"  "D4"  "E4"  "Gb4" "G4"  "A4"  "B4"  "C5"
+#' to:
+#' "C4"  "D4"  "E4"  "F#4" "G4"  "A4"  "B4"  "C5"
+#' or convert the A4 major from:
+#' "A4"  "B4"  "Db5" "D5"  "E5"  "Gb5" "Ab5" "A5"
+#' to:
+#' "A4"  "B4"  "C#5" "D5"  "E5"  "F#5" "G#5" "A5"
+#'
+#' @param notes String, vector: Notes to format
+#' @examples
+#' formatNotation(c("Db4", "D4", "E4", "Gb4", "G4", "A4", "B4", "C5"))
+#' @author Efstathios D. Gennatas
+#' @export
+
+formatNotation <- function(notes) {
+
+  note.letters <- LETTERS[seq(7)]
+
+  # Get letters
+  a <- 1
+  while (a < length(notes)) {
+    i <- a
+    a <- a + 1
+    .notes <- substring(notes, 1, 1)
+    .match <- which(.notes[i] == .notes[-i])[1] + 1
+    .diff <- if (!is.na(.match)) {
+      note1 <- gsub(".$", "", notes[i])
+      note2 <- gsub(".$", "", notes[.match])
+      index <- c(i, .match)
+      note1 != note2
+    } else {
+      FALSE
+    }
+    if (.diff) {
+      # find which is b
+      is.b <- grep(pattern = "b", ignore.case = FALSE, x = c(note1, note2))
+      .change <- index[is.b]
+      note.tochange <- notes[.change]
+      octave <- substring(note.tochange, nchar(note.tochange))
+      new.note.letter <- note.letters[which(note.letters == .notes[.change]) - 1]
+      if (length(new.note.letter) == 0) new.note.letter <- "G"
+      new.note <- paste0(new.note.letter, "#", octave)
+      notes[.change] <- new.note
+    }
+  }
+
+  notes
+
+} # music::formatNotation
---
+++ R/play.R
@@ -0,0 +1,203 @@
+# play.R
+# ::music::
+# 2019 Efstathios D. Gennatas egenn.github.io
+
+#' Minimal "Polyphonic" Wave Player
+#'
+#' Play one or more waveforms at the same time using \code{audio::play}
+#'
+#' @param wave Matrix or vector of waveforms. If a matrix, each column should be a waveform to be played
+#' simultaneously
+#' @param sample.rate Integer: Sample rate. Default = 44100
+#' @param plot Logical: If TRUE: plot wave using \link{mplot}.
+#' @examples
+#' \dontrun{
+#' playWave(freq2wave(440))
+#' }
+#' @export
+#' @author Efstathios D. Gennatas
+
+playWave <- function(wave,
+                     sample.rate = 44100,
+                     plot = FALSE) {
+  if (is.null(dim(wave))) wave <- matrix(wave, ncol = 1)
+  n.notes <- NCOL(wave)
+  for (i in seq(n.notes)) audio::play(wave[, i], rate = sample.rate)
+
+  if (plot) mplot(wave)
+
+} # music::playWave
+
+
+#' Play frequency
+#'
+#' @inheritParams note2freq
+#' @param frequency Numeric, Vector: Frequency / frequencies to play
+#' @param oscillator String: "sine", "square", "saw". Default = "sine"
+#' @param duration Float: Note duration in beats. Default = 1
+#' @param BPM Integer: Beats per minute. Default = 120
+#' @param sample.rate Integer: Sample rate. Default = 44100
+#' @param attack.time Integer: Attack time. Default = 50 (Helps prevent popping)
+#' @param inner.release.time Integer: Release time, that ends on note OFF (instead of beginning at note OFF).
+#' Default = 50 (Also helps prevent popping)
+#' @param plot Logical: If TRUE, plot waveform
+#' @examples
+#' \dontrun{
+#' playFreq(440)
+#' }
+#' @export
+#' @author Efstathios D. Gennatas
+
+playFreq <- function(frequency,
+                     oscillator = "sine",
+                     duration = rep(1, length(frequency)),
+                     BPM = 120,
+                     sample.rate = 44100,
+                     attack.time = 50,
+                     inner.release.time = 50,
+                     plot = FALSE) {
+
+  wave <- c(mapply(freq2wave,
+                   frequency,
+                   oscillator,
+                   duration,
+                   BPM,
+                   sample.rate,
+                   attack.time,
+                   inner.release.time))
+
+  if (plot) mplot(wave)
+  playWave(wave)
+
+} # music::playFreq
+
+#' Play Note
+#'
+#' @inheritParams playFreq
+#' @inheritParams note2freq
+#' @param note String, Vector: Note(s) to be played, e.g. c("Ab4", "B4")
+#' @param plot Logical: If TRUE, plot notes using \link{cplot.piano}. This support only two octaves;
+#' do not try plotting if your notes span more than two octaves.
+#' @param ... Additional arguments to pass to \link{note2freq}
+#' @examples
+#' \dontrun{
+#' playNote("B4")
+#' }
+#' @export
+#' @author Efstathios D. Gennatas
+
+playNote <- function(note,
+                     oscillator = "sine",
+                     duration = rep(1, length(note)),
+                     BPM = 120,
+                     sample.rate = 44100,
+                     attack.time = 50,
+                     inner.release.time = 50,
+                     A4 = 440,
+                     plot = FALSE, ...) {
+
+  freqs <- note2freq(note,
+                     A4 = A4, ...)
+
+  if (plot) cplot.piano(note)
+
+  playFreq(freqs,
+           oscillator = oscillator,
+           duration = duration,
+           BPM = BPM,
+           sample.rate = sample.rate,
+           attack.time = attack.time,
+           inner.release.time = inner.release.time)
+
+} # music::playNote
+
+
+#' Play Chord
+#'
+#' @inheritParams playNote
+#' @param chord String, vector: Notes making up chord. e.g. c("A4", "C5", "E5").
+#' e.g. output of \link{buildChord}
+#' @param type String: "harmonic", "ascending", "descending". Default = "harmonic"
+#' @param plot Logical: If TRUE, plot chord using \link{cplot.piano}
+#' @export
+#' @return The constructed waveform (invisibly)
+#' @examples
+#' \dontrun{
+#' playChord(buildChord("E4", "minor"))
+#' }
+#' @author Efstathios D. Gennatas
+
+playChord <- function(chord,
+                      type = c("harmonic", "ascending", "descending"),
+                      oscillator = "sine",
+                      duration = 1,
+                      sample.rate = 44100,
+                      attack.time = 50,
+                      inner.release.time = 50,
+                      A4 = 440,
+                      plot = FALSE, ...) {
+
+  type <- match.arg(type)
+
+  wave <- lapply(chord, function(i) freq2wave(note2freq(i, A4 = A4, ...),
+                                              oscillator = oscillator,
+                                              duration = duration,
+                                              sample.rate = sample.rate,
+                                              attack.time = attack.time,
+                                              inner.release.time = inner.release.time))
+
+  wave <- switch(type,
+                 harmonic = do.call(cbind, wave),
+                 ascending = do.call(c, wave),
+                 descending = do.call(c, rev(wave)))
+
+  if (plot) cplot.piano(chord)
+  playWave(wave)
+  invisible(wave)
+
+} # music::playChord
+
+
+#' Play Progression
+#'
+#' @inheritParams playNote
+#' @param progression List of string vectors: Each element of the list is a chord.
+#' e.g. output of \link{buildProgression}
+#' @param plot Logical. If TRUE, plot each chord in the progression using \link{cplot.piano}
+#' @export
+#' @examples
+#' \dontrun{
+#' playProgression(buildProgression("G4", "minor"))
+#' }
+#' @author Efstathios D. Gennatas
+
+playProgression <- function(progression,
+                            oscillator = c("sine", "square", "saw", "triangle"),
+                            duration = 1,
+                            BPM = 120,
+                            sample.rate = 44100,
+                            attack.time = 50,
+                            inner.release.time = 50,
+                            A4 = 440,
+                            plot = FALSE, ...) {
+
+  oscillator <- match.arg(oscillator)
+  wave <- lapply(progression, function(i)
+    do.call(cbind,
+            lapply(i, function(j) freq2wave(note2freq(j, A4 = A4, ...),
+                                            oscillator = oscillator,
+                                            duration = duration,
+                                            BPM = BPM,
+                                            sample.rate = sample.rate,
+                                            attack.time = attack.time,
+                                            inner.release.time = inner.release.time))))
+  wave <- do.call(rbind, wave)
+  playWave(wave)
+  if (plot) {
+    for (i in seq(progression)) {
+      cplot.piano(progression[[i]])
+      cat("\n")
+    }
+  }
+
+} # playProgression
